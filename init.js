6a7e11c79286305c78620b12f9fa89f79bd38f41
KartikPandey19 <kartik2000pandey@gmail.com>
2021-05-12 15:23:11 +0530

    up to grid

--- init.js
+++ init.js
@@ -1,18 +1,30 @@
 // initially grid building 
-let colums = 26;
+let columns = 26;
 let rows = 100;
 
 let topRow = document.querySelector(".top-row");
-let leftcol =document.querySelector(".left-col");
-for(let i= 0;i< colums;i++){
+let leftcol = document.querySelector(".left-col");
+let grid = document.querySelector(".grid");
+for (let i = 0; i < columns; i++) {
      let div = document.createElement("div")
      div.innerText = String.fromCharCode(65 + i);
      div.setAttribute("class", "cell");
      topRow.appendChild(div);
 }
-for(let i= 1;i<=rows;i++){
+for (let i = 1; i <= rows; i++) {
      let div = document.createElement("div")
      div.innerText = i;
-     div.setAttribute("class","cell");
-     leftCol.appendChild(div); 
-} 
+     div.setAttribute("class", "block");
+     leftcol.appendChild(div);
+}
+for (let i = 0; i < rows; i++) {
+     let row = document.createElement("div");
+     row.setAttribute("class", "row");
+     for (let j = 0; j < columns; j++) {
+          let div = document.createElement("div")
+          div.innerText = `${i} ${String.fromCharCode(65 + j)}`;
+          div.setAttribute("class", "cell");
+          row.appendChild(div);
+     }
+     grid.appendChild(row);
+}
